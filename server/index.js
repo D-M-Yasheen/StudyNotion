4b1be426f726d8de776247874025d2e458b462a0
D.M. Yasheen <yasheenkhandm@gmail.com>
2023-12-10 19:21:08 +0530

    first commit

--- server/index.js
+++ server/index.js
@@ -23,8 +23,8 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
     cors({
-        origin: "http://localhost:3000",
-        // origin: "*",
+        // origin: "http://localhost:3000",
+        origin: "*",
         credentials: true,
     })
 )
